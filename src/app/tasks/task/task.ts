cb68b18478ebbc83c4b839fb65b6d019025902f2
Reda <mahmoudreda457@gmail.com>
2026-03-01 23:20:37 +0200

    DatePipe

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task as TaskModel } from './task.model';
 import { Card } from '../../shared/card/card';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
