a7a26f16aa5aed13075ab1197b0b1185e69e38e0
Reda <mahmoudreda457@gmail.com>
2026-03-01 15:35:13 +0200

    Tasks component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,24 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { User } from './user/user';
 import { DUMMY_USERS } from './user/dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [User, HeaderComponent],
+  imports: [User, HeaderComponent, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-playground');
   protected readonly users = signal(DUMMY_USERS);
+  selectedUserId = signal<string | undefined>('u1');
+
+  get selectedUser() {
+    return this.users().find(user => user.id === this.selectedUserId())!;
+  }
 
   onSelectUser(id: string) {
-    console.log(id);
+    this.selectedUserId.set(id);
   }
 }
