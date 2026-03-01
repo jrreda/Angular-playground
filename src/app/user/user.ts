c413d2135b622e5a168ec0d66097b4154710e0e1
Reda <mahmoudreda457@gmail.com>
2026-03-01 23:18:25 +0200

    Card Component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Card } from '../shared/card/card';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [Card],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
